569a21e4b9d9997eb25a59330deed33611832a21
leonawicz <matt.rstat@gmail.com>
2018-03-14 13:11:53 -0600

    Update .onLoad.

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,13 @@
   .tabr_env$opts <- list(dev = "pdf", midi = TRUE)
   lp_path <- Sys.which("lilypond")
   if(lp_path == ""){
-    tabr_options(lilypond = "lilypond")
-  } else {
-    tabr_options(lilypond = lp_path)
+    lp_path <- "lilypond"
+    if(.Platform$OS.type == "windows"){
+      opts <- c("C:/Program Files (x86)/LilyPond/usr/bin/lilypond.exe",
+                "C:/Program Files/LilyPond/usr/bin/lilypond.exe")
+      if(file.exists(opts[1])) lp_path <- opts[1]
+      if(file.exists(opts[2])) lp_path <- opts[2]
+    }
   }
+  tabr_options(lilypond = lp_path)
 }
