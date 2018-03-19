d7482fbceb76175cc2ac714e9d154baddc5412e3
leonawicz <matt.rstat@gmail.com>
2018-03-18 23:39:30 -0600

    Fix typo.

--- R/phrase.R
+++ R/phrase.R
@@ -24,7 +24,7 @@
 #'
 #' Opening and closing slurs (or hammer ons and pull offs) are indicated with opening and closing parentheses, slides with \code{-}, and simple bends with \code{^}.
 #' Text annotations aligned vertically with a note in time on the staff is done by appending the text to the note info entry itself. See \code{\link{notate}}.
-#' FOr more details and example, see the package vignettes.
+#' For more details and example, see the package vignettes.
 #'
 #' @param notes character, notes \code{a} through \code{g}. See details.
 #' @param info character, metadata pertaining to the \code{notes }. See details.
