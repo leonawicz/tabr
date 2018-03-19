b3ce9ac07a1ae68fe918459b7fb17b4075f6090d
leonawicz <matt.rstat@gmail.com>
2018-03-19 00:21:30 -0600

    Add unit tests.

--- tests/testthat/test-transpose.R
+++ tests/testthat/test-transpose.R
@@ -12,4 +12,9 @@ test_that("transpose returns as expected.", {
   expect_equal(tp(x[[3]], 2, key = "g"), "b3 c#5 d5")
   expect_equal(tp("a b' c''", 2, key = "f"), "b3 d_5 d5")
   expect_equal(tp("a, b c'", 2, key = "g"), "b2 c#4 d4")
+
+  expect_equal(tp("r a, b r c'", 2, key = "g"), "r b2 c#4 r d4")
+  expect_equal(tp("r a,~ a, b r c'~", 2, key = "g"), "r b2~ b2 c#4 r d4~")
+
+  expect_error(tp("x", "`x` is not a valid string of notes."))
 })
