9a6894310111bc26962a66720d4e3f13f9ff7f74
leonawicz <matt.rstat@gmail.com>
2018-03-21 13:54:08 -0600

    Update documentation and unit tests.

--- tests/testthat/test-phrase.R
+++ tests/testthat/test-phrase.R
@@ -11,16 +11,19 @@ test_that("phrase returns as expected", {
 
   expect_equal(as.character(p("de_' aa_,", 1)), "<d es'>1 <a as,>1")
   expect_equal(as.character(p("ee_' a_a#,", "2")), "<e es'>2 <as ais,>2")
-  expect_equal(as.character(p("d,, e_e_ a_' b_'d", "1 2 4.. 8.")), "<d,,>1 <eses>2 <as'>4.. <bes' d>8.")
-  expect_equal(as.character(p("d,, e_e_ a_' b_'d", "1 2 4.. 8.", abb = FALSE)), "<d,,>1 <eesees>2 <aes'>4.. <bes' d>8.")
+  expect_equal(as.character(p("d,, e_e_ a_' b_'d", "1 2 4.. 8.")), "<d,,>1 <es es>2 <as'>4.. <bes' d>8.")
+  expect_equal(as.character(p("d,, e_e_ a_' b_'d", "1 2 4.. 8.", abb = FALSE)),
+               "<d,,>1 <ees ees>2 <aes'>4.. <bes' d>8.")
 
   y <- "<d,,\\1>1 <es\\2 es\\1>2 <as'\\3>4.. <bes'\\4 d\\3>8."
   expect_equal(as.character(p("d,, e_e_ a_' b_'d", "1 2 4.. 8.", "1 21 3 43")), y)
   expect_equal(as.character(p("d1 e_3e_ a_' b_'d", "1 2 4.. 8.", "1 21 3 43")), y)
+
+  n <- "d,,e_e_3 a_4b_4d"
   y <- "<d,,\\3 es\\2 es\\1>2 <as'\\3 bes'\\2 d\\1>2"
-  expect_equal(as.character(p("d,,e_e_3 a_4b_4d", "2 2", "321 321")), y)
-  expect_equal(as.character(p("d,,e_e_3 a_4b_4d", "2*2", "321*2")), y)
-  expect_equal(as.character(p("d,,e_e_3 a_4b_4d", 2, "321 3s")), y)
+  expect_equal(as.character(p(n, "2 2", "321 321")), y)
+  expect_equal(as.character(p(n, "2*2", "321*2")), y)
+  expect_equal(as.character(p(n, 2, "321 3s")), y)
 
   x1 <- phrase("c b, c", "4. 8( 8)", "5 5 5") # direction implies hammer on
   x2 <- phrase("b2 c d", "4( 4)- 2", "5 5 5") # hammer and slide
